39dadb26b8746c3beb546ca4e5c664641b3a6d0c
mukesh.sen <mukesh.sen@YB1012457LT.Yash.com>
2021-08-24 15:11:34 +0530

    first commit

---
+++ internsDMS/src/main/java/com/yash/entity/Intern.java
@@ -0,0 +1,64 @@
+package com.yash.entity;
+
+import java.util.Date;
+
+public class Intern {
+	
+	private int internId;
+	private String internFirstName;
+	private String internLastName;
+	private Date internDOB;
+	private Date internHireDate;
+	
+	public int getInternId() {
+		return internId;
+	}
+	public void setInternId(int internId) {
+		this.internId = internId;
+	}
+	public String getInternFirstName() {
+		return internFirstName;
+	}
+	public void setInternFirstName(String internFirstName) {
+		this.internFirstName = internFirstName;
+	}
+	public String getInternLastName() {
+		return internLastName;
+	}
+	public void setInternLastName(String internLastName) {
+		this.internLastName = internLastName;
+	}
+	public Date getInternDOB() {
+		return internDOB;
+	}
+	public void setInternDOB(Date internDOB) {
+		this.internDOB = internDOB;
+	}
+	public Date getInternHireDate() {
+		return internHireDate;
+	}
+	public void setInternHireDate(Date internHireDate) {
+		this.internHireDate = internHireDate;
+	}
+	public Intern(int internId, String internFirstName, String internLastName, Date internDOB, Date internHireDate) {
+		super();
+		this.internId = internId;
+		this.internFirstName = internFirstName;
+		this.internLastName = internLastName;
+		this.internDOB = internDOB;
+		this.internHireDate = internHireDate;
+	}
+	public Intern() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Intern [internId=" + internId + ", internFirstName=" + internFirstName + ", internLastName="
+				+ internLastName + ", internDOB=" + internDOB + ", internHireDate=" + internHireDate + "]";
+	}
+	
+	
+	
+
+}
---
+++ internsDMS/src/main/java/com/yash/internsDMS/App.java
@@ -0,0 +1,49 @@
+package com.yash.internsDMS;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * 
+ * @author mukesh.sen
+ *
+ */
+public class App {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("INTERNS DMS STARTED.................");
+		
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		boolean go= true;
+		
+		while (go) {
+			System.out.println("****** Press 1 for Register new Interns *******");
+			System.out.println("****** Press 2 for View All Interns *******");
+			System.out.println("****** Press 3 for view single Interns *******");
+			System.out.println("****** Press 4 for Update Interns *******");
+			System.out.println("****** Press 5 for Delete Interns *******");
+			System.out.println("****** Press 6 for Exit App *******");
+			
+			try {
+				int input= Integer.parseInt(br.readLine());
+				switch (input) {
+				case 1:
+					//Add new Interns
+					// taking input from users
+					System.out.println("Enter Intern ID: ");
+					int iId = Integer.parseInt(br.readLine());
+					
+					break;
+
+				default:
+					break;
+				}
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+	}
+
+}
